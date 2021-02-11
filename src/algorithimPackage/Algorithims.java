36879ac684fd2830fbb7b79c8427ac3f4d183d40
Anon214 <43688027+Anon214@users.noreply.github.com>
2021-02-10 21:50:35 -0500

    adding heap sort

--- src/algorithimPackage/Algorithims.java
+++ src/algorithimPackage/Algorithims.java
@@ -1,8 +1,23 @@
 package algorithimPackage;
 
+
 public class Algorithims {
 
-	public static void HeapSort() {
+	public static int[] HeapSort(int[] array) {
+		int temp = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				if (array[i] < array[j]) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+		
+		return array;
+		
 		
 	}
 
