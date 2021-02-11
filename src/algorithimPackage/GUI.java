817f13aced51b82342cf4a1f6d26db558f91c85a
Anon214 <43688027+Anon214@users.noreply.github.com>
2021-02-10 21:37:31 -0500

    latest failure

--- src/algorithimPackage/GUI.java
+++ src/algorithimPackage/GUI.java
@@ -5,16 +5,14 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class GUI extends JPanel{
-	
 	static int screenWidth = 1276;
 	static int screenHeight = 720;
 	
@@ -25,11 +23,9 @@ public class GUI extends JPanel{
 		this.setBackground(Color.darkGray);
 		int width = 10;
 		int x = 0;
-		g.setColor(Color.white);
-		
-		//draws rectangles
 		int listArr[] = shuffleRectangles(width);
 		
+		//draws rectangles
 		for (int i = 0; i < (screenWidth - 15) / width; i++) {
 			g.setColor(Color.white);
 			g.fillRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
@@ -38,8 +34,26 @@ public class GUI extends JPanel{
 			x += width;
 		}
 		
-		System.out.println("Array: " + Arrays.toString(listArr));
 		
+
+		listArr = Algorithims.HeapSort(listArr);
+		
+		System.out.println("Array: " + Arrays.toString(listArr));
+		for (int i = 0; i < (screenWidth - 15) / width; i++) {
+			g.setColor(Color.white);
+			g.fillRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
+			g.setColor(Color.black);
+			g.drawRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
+			x += width;
+		}
+		
+		refill(listArr);
+		
+	}
+	
+	public void refill(int[] listArr) {
+		listArr = Algorithims.HeapSort(listArr);
+		repaint();
 	}
 
 	public static void main(String[] args) {
@@ -53,7 +67,6 @@ public class GUI extends JPanel{
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-//		System.out.println(Arrays.toString(shuffleRectangles(10)));
 		frame.add(guiObj);
 
 	}
