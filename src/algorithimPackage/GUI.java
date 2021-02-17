c9f3a2dffa3da9524d6482a4593d11d94d103baa
Anon214 <43688027+Anon214@users.noreply.github.com>
2021-02-17 13:31:08 -0500

    fixed visualizer

--- src/algorithimPackage/GUI.java
+++ src/algorithimPackage/GUI.java
@@ -2,6 +2,7 @@ package algorithimPackage;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -15,45 +16,49 @@ import javax.swing.JPanel;
 public class GUI extends JPanel{
 	static int screenWidth = 1276;
 	static int screenHeight = 720;
-	
+	boolean shuffled = false;
+
 	public void paintComponent(Graphics g) {
 		//height has 40 pixel difference
 		//width has 15 pixel difference
 		super.paintComponent(g);
 		this.setBackground(Color.darkGray);
+		Graphics2D g2d = (Graphics2D) g;
+		
 		int width = 10;
 		int x = 0;
 		int listArr[] = shuffleRectangles(width);
 		
 		//draws rectangles
-		for (int i = 0; i < (screenWidth - 15) / width; i++) {
-			g.setColor(Color.white);
-			g.fillRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
-			g.setColor(Color.black);
-			g.drawRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
-			x += width;
-		}
-		
 		
 
-		listArr = Algorithims.HeapSort(listArr);
-		
-		System.out.println("Array: " + Arrays.toString(listArr));
-		for (int i = 0; i < (screenWidth - 15) / width; i++) {
-			g.setColor(Color.white);
-			g.fillRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
-			g.setColor(Color.black);
-			g.drawRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
-			x += width;
+		if (shuffled == false) {
+			for (int i = 0; i < (screenWidth - 15) / width; i++) {
+				System.out.print("Went through");
+				g2d.setPaint(Color.white);
+				g2d.fillRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
+				g2d.setPaint(Color.black);
+				g2d.drawRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
+				x += width;
+			}
 		}
-		
-		refill(listArr);
+		else {
+			listArr = Algorithims.HeapSort(listArr);
+		}
+		refill(g2d, listArr, width, x);
 		
 	}
 	
-	public void refill(int[] listArr) {
-		listArr = Algorithims.HeapSort(listArr);
-		repaint();
+	public void refill(Graphics2D g2d, int listArr[], int width, int x) {
+		this.shuffled = true;
+		for (int i = 0; i < (screenWidth - 15) / width; i++) {
+			g2d.setPaint(Color.white);
+			g2d.fillRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
+			g2d.setPaint(Color.black);
+			g2d.drawRect(x, (screenHeight - listArr[i]) - 40, width, listArr[i]);
+			x += width;
+		}
+
 	}
 
 	public static void main(String[] args) {
